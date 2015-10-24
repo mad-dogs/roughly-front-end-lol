e5859ca31a71458acb964c37b4e77b9f0cc5aeca
Matt Gray <matt.gray@retrofuzz.com>
2015-10-24 15:45:29 +0100

    Basic controllers for maps

--- app/map/map.js
+++ app/map/map.js
@@ -1,14 +1,40 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute'])
+angular.module('myApp.map', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/map', {
     templateUrl: 'map/map.html',
-    controller: 'MapCtrl'
+    controller: 'MapPageCtrl'
   });
 }])
 
-.controller('MapCtrl', [function() {
+.controller('MapPageCtrl', [function() {
+
+	
+
+}])
+
+.controller('TopNavController', ['$scope', function($scope) {
+
+	
+
+}])
+
+.controller('ContentBelowController', ['$scope', function($scope) {
+
+	
+
+}])
+
+.controller('BottomActionsController', ['$scope', function($scope) {
+
+	
+
+}])
+
+.controller('MapController', ['$scope', function($scope) {
+
+	
 
 }]);
